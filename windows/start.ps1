85030f4d79168decc55419f65da72b607168b69c
Nhu Phuc <phucho0237@gmail.com>
2023-12-12 20:37:25 +0700

    Create new directory instead of TEMP

--- windows/start.ps1
+++ windows/start.ps1
@@ -1,8 +1,10 @@
+New-Item -ItemType Directory -Path C:\Infrastructure
+
 $githubScriptUrl = "https://raw.githubusercontent.com/phucho0237/infrastructure/main/windows/setup.ps1"
 
 $scriptContent = Invoke-RestMethod -Uri $githubScriptUrl
 
-$scriptPath = "$env:TEMP\Setup.ps1"
+$scriptPath = "C:\Infrastructure\Setup.ps1"
 $scriptContent | Out-File -FilePath $scriptPath -Encoding UTF8
 
 Start-Process -FilePath "powershell.exe" -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$scriptPath`"" -Verb RunAs
