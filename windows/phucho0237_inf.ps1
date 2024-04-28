1be145f7b2b23ec63d89d6a4633316503de90fde
Nhu Phuc <phucho0237@gmail.com>
2024-04-29 05:53:03 +0700

    directx

--- windows/phucho0237_inf.ps1
+++ windows/phucho0237_inf.ps1
@@ -6,7 +6,8 @@ if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdenti
 # Create Temporary Folder At %temp%
 $tempFolder = Join-Path -Path $env:TEMP -ChildPath "infrastructure"
 if (-not (Test-Path -Path $tempFolder)) {
-    New-Item -ItemType Directory -Path $tempFolder | Out-Null    
+    New-Item -Path $tempFolder -ItemType Directory  | Out-Null
+        
     Write-Host `[INIT] Created a new folder at $tempFolder`
 } else {
     Write-Host `[INIT] Folder existed at $tempFolder`
@@ -16,14 +17,39 @@ if (-not (Test-Path -Path $tempFolder)) {
 $chocoFolder = Join-Path -Path C:\ProgramData -ChildPath "chocolatey"
 
 if (Test-Path -Path $chocoFolder) {
-    Write-Host "[PKG] Chocolatey was installed in this system. Skipping..."
+    Write-Host "[INIT] Chocolatey was installed in this system. Skipping..."
 }
 else {
     # Install Chocolatey
-    Write-Host "[PKG] Chocolatey not detected, installing..."
+    Write-Host "[INIT] Chocolatey not detected, installing..."
+
     [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
     iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-    Write-Host "[PKG] Chocolatey installation completed"
+
+    Write-Host "[INIT] Chocolatey installation completed"
 }
+
+# Package Install
+Write-Host "[PKG] Started package install..."
+
+## DirectX
+$pkgFolder = Join-Path -Path $tempFolder -ChildPath "pkg"
+if (-not (Test-Path -Path $pkgFolder)) {
+    New-Item -Path $pkgFolder -ItemType Directory | Out-Null
+}
+
+Write-Host "[PKG] Installing DirectX..."
+
+$dxDownloadPath = Join-Path -Path $pkgFolder -ChildPath "dxwebsetup.exe"
+
+if (-not (Test-Path -Path $dxDownloadPath)) {
+    $directxUrl = "https://download.microsoft.com/download/1/7/1/1718CCC4-6315-4D8E-9543-8E28A4E18C4C/dxwebsetup.exe"
+    Invoke-Webrequest -Uri $directxUrl -OutFile $dxDownloadPath
+}
+
+# Wait 5 seconds
+Start-Sleep -Seconds 3
+
+Start-Process -FilePath $dxDownloadPath -Wait
 
 Pause
