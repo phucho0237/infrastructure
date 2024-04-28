958e6cd7f5393d492453b6528eba5016b7f355b4
Nhu Phuc <phucho0237@gmail.com>
2024-04-28 17:59:30 +0700

    Create phucho0237_inf.ps1

---
+++ windows/phucho0237_inf.ps1
@@ -0,0 +1,30 @@
+## RUN THIS FIRST
+# Set-ExecutionPolicy Bypass -Force
+
+# Request Administrators Permission
+if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+}
+
+# Create Temporary Folder At %temp%
+$tempFolder = Join-Path -Path $env:TEMP -ChildPath "infrastructure"
+if (-not (Test-Path -Path $tempFolder)) {
+    New-Item -ItemType Directory -Path $tempFolder | Out-Null    
+    Write-Host `Created a new folder at $tempFolder`
+} else {
+    Write-Host `Folder existed at $tempFolder`
+}
+
+# Check if chocolatey is installed or not
+$chocoFolder = Join-Path -Path C:\ProgramData -ChildPath "chocolatey"
+
+if (Test-Path -Path $chocoFolder) {
+    Write-Host `Chocolatey was installed in this system. Skipping...
+}
+else {
+    # Install chocolatey
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+    Write-Host "Chocolatey installation completed."
+}
+
+Pause
